c19a0b93dadca21f95e654dec1dddd6d9f5a7eba
shihan00321 <shihan001@naver.com>
2025-08-11 17:56:50 +0900

    6,7단계 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,9 +8,9 @@ fun main() {
     println("== 명언 앱 ==")
     while (true) {
         print("명령) ")
-        val input = readlnOrNull()
-        when(input) {
-            "등록" -> {
+        val input = readlnOrNull()?.trim() ?: continue
+        when {
+            input == "등록" -> {
                 print("명언 : ")
                 val quote = readln()
                 print("작가 : ")
@@ -19,12 +19,22 @@ fun main() {
                 wiseSayings.add(wiseSaying)
                 println("${id++}번 명언이 등록되었습니다.")
             }
-            "목록" -> {
+            input == "목록" -> {
                 println("번호 / 작가 / 명언")
                 println("-------------------")
                 wiseSayings.reversed().forEach { println("${it.id} / ${it.author} / ${it.quote}") }
             }
-            "종료" -> {
+            input.startsWith("삭제") -> {
+                val id = input.split("?")[1].split("=")[1].toLong()
+                val deleteWiseSaying = wiseSayings.find { it.id == id}
+                if (deleteWiseSaying == null) {
+                    println("${id}번 명언은 존재하지 않습니다.")
+                    continue
+                }
+                wiseSayings.remove(deleteWiseSaying)
+                println("${id}번 명언이 삭제되었습니다.")
+            }
+            input == "종료" -> {
                 break
             }
             else -> println("알 수 없는 명령입니다.")
