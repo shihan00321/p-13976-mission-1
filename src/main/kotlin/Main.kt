1e4b2d7d3121f2f2185c2f263431718fb6b8f27a
shihan00321 <shihan001@naver.com>
2025-08-11 17:24:51 +0900

    2번째 완료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,20 @@ package com
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println("== 명언 앱 ==")
-    print("명령) ")
-    val input = readlnOrNull()
+    while (true) {
+        print("명령) ")
+        val input = readlnOrNull()
+        when(input) {
+            "등록" -> {
+                print("명언 : ")
+                val quote = readlnOrNull()
+                print("작가 : ")
+                val author = readlnOrNull()
+            }
+            "종료" -> {
+                break
+            }
+            else -> println("알 수 없는 명령입니다.")
+        }
+    }
 }
